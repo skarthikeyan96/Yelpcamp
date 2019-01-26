9f2b67faedc145ea2a4da659419dbd4f558f627e
Karthikeyan <imkarthikeyans@gmail.com>
2019-01-26 18:46:59 +0530

    added authentication middleware

---
+++ middleware/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    isLoggedIn : (req, res, next) =>{
+        if (req.isAuthenticated()) {
+          return next()
+        }
+        req.flash("error","Please login first")
+        res.redirect('/')
+      }
+}
